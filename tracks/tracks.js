a84dfd14e1106589a53178c1c5a677ec6133e401
Jonathan Roley <jonathan.roley@gmail.com>
2024-10-01 11:15:47 -0400

    fix ext

--- tracks/tracks.js
+++ tracks/tracks.js
@@ -1,12 +1,4 @@
 (() => (window._AFTracks = {
-  "kangaroo": {
-    "name": "Kangaroo",
-    "path": "tracks/kangaroo.txt"
-  },
-  "epoq": {
-    "name": "Epoq",
-    "path": "tracks/epoq.txt"
-  },
   "epoq-lepidoptera": {
     "name": "Epoq-Lepidoptera",
     "path": "tracks/epoq-lepidoptera.js"
