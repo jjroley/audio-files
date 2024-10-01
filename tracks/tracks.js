d75d28db2797fe4fbe1947053bff4fe558df3d7a
Jonathan Roley <jonathan.roley@gmail.com>
2024-10-01 11:51:43 -0400

    add kg song

--- tracks/tracks.js
+++ tracks/tracks.js
@@ -1,6 +1,10 @@
-(() => (window._AFTracksList = {
+(() => (window._AFTracks = {
   "epoq-lepidoptera": {
     "name": "Epoq-Lepidoptera",
     "path": "tracks/epoq-lepidoptera.js"
+  },
+  "kangaroo-game": {
+    "name": "Kangaroo-Game",
+    "path": "tracks/kangaroo-game.js"
   }
 }))()
